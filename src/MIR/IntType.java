3bad5b791a048d7d3fedbc67f24d0b3ea3d71a0d
was_n <2360402125@qq.com>
2021-11-30 18:15:20 +0800

    IR

--- src/MIR/IntType.java
+++ src/MIR/IntType.java
@@ -3,6 +3,23 @@ package MIR;
 public class IntType extends BaseType{
     public int size;
     public IntType(int size) {this.size=size;}
-    @Override public String toString(){return "i32";}
-    @Override public int size(){return 4;}
+    @Override public String toString(){if(size==4)return "i32";else return "i1";}
+    @Override public int size(){return size;}
 }
+
+a = 1
+if (cond) {
+    a = 2
+} else {
+    a = 3
+}
+b = a
+
+%1 = 1
+if (cond) {
+    %2 = 2
+} else {
+    %3 = 3
+}
+a4 = phi(from which branch, a2, a3)
+b = a4
