316da1696ca2eaf3a90c2c7c20e30268151c23bd
was_n <2360402125@qq.com>
2022-01-17 22:40:35 +0800

    IR

--- src/Util/ClassType.java
+++ src/Util/ClassType.java
@@ -6,7 +6,9 @@ public class ClassType extends Type{
     public String identifier;
     public HashMap<String,VarSymbol> vars = new HashMap<String,VarSymbol>();
     public HashMap<String,FuncSymbol> funcs = new HashMap<String, FuncSymbol>();
+    public HashMap<String, Integer> mapping = new HashMap<String, Integer>();
     public FuncSymbol Constructor;
+    public int num = 0;
     public ClassType(String id) {
         this.identifier=id;
     }
@@ -14,5 +16,8 @@ public class ClassType extends Type{
     public boolean equal(Type tp) {
         if(tp.is_null()) return true;
         else return ((tp instanceof ClassType) && (identifier.equals(((ClassType) tp).identifier)));
+    }
+    public int getNumber(String name) {
+        return mapping.get(name);
     }
 }
